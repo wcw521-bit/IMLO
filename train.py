51b4a501177831f4ded13e03522d7c0ec1e70b06
Maida <wcw521@york.ac.uk>
2026-05-17 23:43:16 +0100

    Update train.py

--- train.py
+++ train.py
@@ -35,10 +35,14 @@ def get_transforms(train=True):
     if train:
         return transforms.Compose([
             transforms.Resize((IMAGE_SIZE + 32, IMAGE_SIZE + 32)),
-            transforms.RandomResizedCrop(IMAGE_SIZE, scale=(0.8, 1.0)),
+            transforms.RandomResizedCrop(IMAGE_SIZE, scale=(0.7, 1.0)),
             transforms.RandomHorizontalFlip(),
             #transforms.RandomRotation(15),
-            #transforms.ColorJitter(0.3, 0.3, 0.3, 0.1),
+            transforms.ColorJitter(
+                brightness = 0.1,
+                contrast = 0.1,
+                saturation = 0.1,
+                hue = 0.2),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
